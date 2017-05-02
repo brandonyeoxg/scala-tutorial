0818c7206876c0a7f0e3898d989f193f0f468424
brandonyeoxg <brandonyeoxg@gmail.com>
2017-05-02 20:52:56 +0800

    Add class functionalities

--- src/Main.scala
+++ src/Main.scala
@@ -1,5 +1,20 @@
+import java.io._
 
+class Point(val xc : Int, val yc: Int) {
+    var x : Int = xc
+    var y : Int = yc
+    
+    def move(dx : Int, dy : Int) {
+        x = x + dx
+        y = y + dy
+        println("Point x location : " + x)
+        println("Point y location : " + y)
+    }
+}
 
-class Main {
-  
+object Main {
+    def main(args : Array[String]) {
+        val pt = new Point(10, 20)
+        pt.move(10, 10);
+    }
 }
