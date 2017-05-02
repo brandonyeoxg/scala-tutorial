7b43778ffd9955b4781d0df702f0c0d0db62f5e9
brandonyeoxg <brandonyeoxg@gmail.com>
2017-05-02 20:59:09 +0800

    Add extend functionality

--- src/Main.scala
+++ src/Main.scala
@@ -12,9 +12,22 @@ class Point(val xc : Int, val yc: Int) {
     }
 }
 
+class Location(override val xc : Int, override val yc : Int, val zc : Int) extends Point(xc, yc) {
+    var z : Int = zc
+    
+    def move(dx : Int, dy : Int, dz : Int) {
+        x = x + dx
+        y = y + dy
+        z = z + dz
+        
+        println("Point x location : " + x + "\nPoint y location: " + y + "\nPoint z location : " + z);
+    }
+}
+
 object Main {
     def main(args : Array[String]) {
-        val pt = new Point(10, 20)
-        pt.move(10, 10);
+        val loc = new Location(10, 20, 15)
+        
+        loc.move(10, 10, 5);
     }
 }
