3a96526c7dabb99a3ed80bd774d9b1797dc4aeeb
brandonyeoxg <brandonyeoxg@gmail.com>
2017-05-02 20:35:11 +0800

    Finish hello world exercise

--- src/Main.scala
+++ src/Main.scala
@@ -1,5 +1,10 @@
 
-
-class Main {
-  
+/**
+ * This is my first scala program
+ * Prints Hello world as the output
+ */
+object Main {
+    def main(args: Array[String]) {
+        println("Hello World")
+    }
 }
