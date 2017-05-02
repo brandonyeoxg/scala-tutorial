c53eeba8908cd0cf544b77ba76b9b43b1af3c8aa
brandonyeoxg <brandonyeoxg@gmail.com>
2017-05-02 22:13:06 +0800

    Add String formatters

--- src/Main.scala
+++ src/Main.scala
@@ -1,4 +1,18 @@
 object Main {
+    val greeting : String = "Hello, world!" 
     def main(args : Array[String]) {
+        var floatVar = 12.456
+        var intVar = 2000
+        var stringVar = "Hello, Scala!"
+        
+        var fs = printf("The value of the float variable is " + "%f,\n while the value of the integer " 
+                + "%d \nand the value of the string varaible is %s", floatVar, intVar, stringVar) 
+        println(fs);
+        
+        val name = "James"
+        println(s"Hello, $name")
+        println(s"1 + 1 = ${1 + 1}")
+        val height = 1.9d
+        println(f"$name%s is $height%2.2f meters tall")
     }
 }
