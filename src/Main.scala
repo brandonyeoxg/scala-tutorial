e7fdc20321dbf688bece250db1b30ca70f2c1472
brandonyeoxg <brandonyeoxg@gmail.com>
2017-05-02 22:55:01 +0800

    Add trait functionality

--- src/Main.scala
+++ src/Main.scala
@@ -1,4 +1,23 @@
+trait Equal {
+    def isEqual(x : Any) : Boolean
+    def isNotEqual(x : Any) : Boolean = !isEqual(x)
+}
+
+class Point(_x : Int, _y : Int) extends Equal {
+    var x : Int = _x
+    var y : Int = _y
+    
+    def isEqual(obj : Any) = obj.isInstanceOf[Point] && obj.asInstanceOf[Point].x == x && obj.asInstanceOf[Point].y == y
+}
+
 object Main {
     def main(args : Array[String]) {
+        var p1 = new Point(2, 3)
+        var p2 = new Point(2, 4)
+        var p3 = new Point(2, 3)
+        
+        println(p1.isNotEqual(p2))
+        println(p1.isNotEqual(p3))
+        println(p1.isNotEqual(2))
     }
 }
