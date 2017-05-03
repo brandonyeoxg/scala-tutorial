12422a6369ef7c815c37abfe672ba2968b34a817
brandonyeoxg <brandonyeoxg@gmail.com>
2017-05-03 21:09:14 +0800

    Add basic regex functionality

--- src/Main.scala
+++ src/Main.scala
@@ -1,4 +1,8 @@
 object Main {
     def main(args : Array[String]) {
+        val pattern = "Scala".r
+        val str = "Scala is Scalable and cool"
+        
+        println(pattern findFirstIn str)
     }
 }
