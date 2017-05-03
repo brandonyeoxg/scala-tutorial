c4fd51d118935665105ab3cc5bfea5e15624a074
brandonyeoxg <brandonyeoxg@gmail.com>
2017-05-03 21:59:20 +0800

    Add file output

--- src/Main.scala
+++ src/Main.scala
@@ -1,4 +1,11 @@
+import java.io.PrintWriter
+import java.io.File
+
 object Main {
     def main(args : Array[String]) {
+        val writer = new PrintWriter(new File("test.txt"))
+        
+        writer.write("Hello Scala")
+        writer.close()
     }
 }
