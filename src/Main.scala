e0886ab7de5b3cad8eb0f35cc3da1f58cc595857
brandonyeoxg <brandonyeoxg@gmail.com>
2017-05-03 20:58:08 +0800

    Add case class functionality

--- src/Main.scala
+++ src/Main.scala
@@ -4,7 +4,20 @@ object Main {
         println(matchTest("two"))
         println(matchTest("test"))
         println(matchTest(1))
+        
+        val alice = new Person("Alice", 25)
+        val bob = new Person("Bob", 32)
+        val charlie = new Person("Charlie", 32)
+        
+        for (person <- List(alice,bob,charlie)) {
+            person match {
+                case Person("Alice", 25) => println("Hi Alice!")
+                case Person("Bob", 32) => println("Hi Bob!")
+                case Person(name, age) => println("Age: " + age + " year, name: " + name + "?")
+            }
+        }
     }
+    case class Person(name: String, age: Int)
     
     def matchTest(x : Any) : Any = x match {
         case 1 => "one"
