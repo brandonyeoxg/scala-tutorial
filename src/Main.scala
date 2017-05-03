e013f3dcef3b6edf99a6119d11099fdd9c303d2a
brandonyeoxg <brandonyeoxg@gmail.com>
2017-05-03 21:56:42 +0800

    Add Apply and Unapply functionality

--- src/Main.scala
+++ src/Main.scala
@@ -1,4 +1,33 @@
 object Main {
     def main(args : Array[String]) {
+    /*
+        println("Apply method: " + apply("Zara", "gmail.com"))
+        println("Unapply method: " + unapply("Zara@gmail.com"))
+        println("Unapply method: " + unapply("Zara Ali"))
+ 	*/
+        val x = Main(5)
+        println(x)
+        
+        x match {
+            case Main(num) => println(x + " is bigger two times than " + num)
+            case _=> println("i cannot calculate")
+        }
     }
+    def apply(x: Int) = x*2
+    def unapply(z: Int): Option[Int] = if (z%2==0) Some(z/2) else None
+    /*
+    def apply(user: String, domain: String): String = {
+        user + "@" + domain
+    }
+    
+    def unapply(str: String): Option[(String, String)] = {
+        val parts = str split "@"
+        
+        if (parts.length == 2) {
+            Some(parts(0), parts(1))
+        } else {
+            None
+        }
+    }
+    */
 }
