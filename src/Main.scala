a6a9237b5acc1bafe6ca845537763216761a3a28
brandonyeoxg <brandonyeoxg@gmail.com>
2017-05-03 22:02:50 +0800

    Add reading of file

--- src/Main.scala
+++ src/Main.scala
@@ -1,15 +1,23 @@
 import java.io.PrintWriter
 import java.io.File
+import scala.io.Source
 
 object Main {
     def main(args : Array[String]) {
+//        // Writing to a file
 //        val writer = new PrintWriter(new File("test.txt"))
 //        
 //        writer.write("Hello Scala")
 //        writer.close()
-        print("Please enter your input: ")
-        val line = Console.readLine
-        println("Thanks, you just typed: " + line)
+
+//        // Reading user input
+//        print("Please enter your input: ")
+//        val line = Console.readLine
+//        println("Thanks, you just typed: " + line)
         
+        println("Following is the content read: ")
+        Source.fromFile("test.txt").foreach {
+            print
+        }
     }
 }
