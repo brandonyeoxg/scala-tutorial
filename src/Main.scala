5cd7d66823b8749104ce9400fb53c2eab88a0297
brandonyeoxg <brandonyeoxg@gmail.com>
2017-05-03 20:42:25 +0800

    Add basic pattern matching

--- src/Main.scala
+++ src/Main.scala
@@ -1,4 +1,15 @@
 object Main {
     def main(args : Array[String]) {
+        println(matchTest(3))
+        println(matchTest("two"))
+        println(matchTest("test"))
+        println(matchTest(1))
+    }
+    
+    def matchTest(x : Any) : Any = x match {
+        case 1 => "one"
+        case "two" => 2
+        case y : Int => "scala.Int"
+        case _ => "many"
     }
 }
