83348d02f40a66f941ad17321e1fb852bb56bbe7
brandonyeoxg <brandonyeoxg@gmail.com>
2017-05-03 22:00:29 +0800

    Add user input functionality

--- src/Main.scala
+++ src/Main.scala
@@ -3,9 +3,13 @@ import java.io.File
 
 object Main {
     def main(args : Array[String]) {
-        val writer = new PrintWriter(new File("test.txt"))
+//        val writer = new PrintWriter(new File("test.txt"))
+//        
+//        writer.write("Hello Scala")
+//        writer.close()
+        print("Please enter your input: ")
+        val line = Console.readLine
+        println("Thanks, you just typed: " + line)
         
-        writer.write("Hello Scala")
-        writer.close()
     }
 }
